894d729f432385feb05cb00c4d2c8c6bd50be397
Trey <85683709+Treyo1928@users.noreply.github.com>
2024-11-12 23:20:33 -0600

    First Push

---
+++ OVRPatch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace TreysLeviGrip
+{
+    [HarmonyPatch(typeof(OVRCameraRig), "UpdateAnchors")]
+    class OVRPatch
+    {
+        static void Postfix(OVRCameraRig __instance)
+        {
+            if (Plugin.leviGrip && !Plugin.flipAnimation)
+            {
+                // Apply the offset to both hands, rotating them in the same direction
+                __instance.leftHandAnchor.Rotate(Plugin.offset);
+                __instance.rightHandAnchor.Rotate(Plugin.offset);
+            }
+            else if (Plugin.flipAnimation)
+            {
+                Plugin.PluginLogger.LogInfo("Flipping");
+
+                // Interpolate rotation for the left hand
+                Vector3 startRotation = Plugin.leviGrip ? Vector3.zero : Plugin.offset;
+                Vector3 endRotation = Plugin.leviGrip ? Plugin.offset : Vector3.zero;
+                Vector3 rotationOffset = Vector3.Lerp(startRotation, endRotation, (Time.time - Plugin.flipStartTime) / Plugin.flipDuration);
+                __instance.leftHandAnchor.Rotate(rotationOffset);
+                
+                startRotation = Plugin.leviGrip ? Vector3.zero : Plugin.offset + new Vector3(0, 0, -360);
+                endRotation = Plugin.leviGrip ? Plugin.offset + new Vector3(0, 0, -360) : Vector3.zero;
+                rotationOffset = Vector3.Lerp(startRotation, endRotation, (Time.time - Plugin.flipStartTime) / Plugin.flipDuration);
+                __instance.rightHandAnchor.Rotate(rotationOffset);
+
+                // End flip animation after specified duration
+                if (Time.time - Plugin.flipStartTime > Plugin.flipDuration) Plugin.flipAnimation = false;
+            }
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,81 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TreysLeviGrip
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string pluginGuid = "aoq.treyslevigrip";
+        public const string pluginName = "Treys Levi Grip";
+        public const string pluginVersion = "1.0.0";
+        internal static BepInEx.Logging.ManualLogSource PluginLogger;
+
+        public static bool leviGrip = false;
+        private static string rotationAxis = "Z";
+        public static Vector3 offset = new Vector3(82, 0, 180);
+        public static bool flipAnimation = false;
+        public static float flipStartTime;
+        public static float flipDuration = 0.25f;
+
+        public void Awake()
+        {
+            PluginLogger = Logger;
+
+            Logger.LogInfo("Plugin TreysLeviGrip is loaded!");
+
+            Harmony harmony = new Harmony("TreysLeviGrip");
+            harmony.PatchAll();
+
+            Logger.LogInfo("Harmony patches applied.");
+        }
+
+        public void Update()
+        {
+            if(OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.LTouch) && !flipAnimation)
+            {
+                flipAnimation = true;
+                leviGrip = !leviGrip;
+                flipStartTime = Time.time;
+            }
+            //    if (Input.GetKey(KeyCode.Z)) rotationAxis = "Z";
+            //    if (Input.GetKey(KeyCode.X)) rotationAxis = "X";
+            //    if (Input.GetKey(KeyCode.Y)) rotationAxis = "Y";
+            //    if (Input.GetKey(KeyCode.UpArrow))
+            //    {
+            //        switch (rotationAxis)
+            //        {
+            //            case "X":
+            //                offset = offset + new Vector3(1, 0, 0);
+            //                break;
+            //            case "Y":
+            //                offset = offset + new Vector3(0, 1, 0);
+            //                break;
+            //            case "Z":
+            //                offset = offset + new Vector3(0, 0, 1);
+            //                break;
+            //        }
+            //    }
+
+            //    if (Input.GetKey(KeyCode.DownArrow))
+            //    {
+            //        switch (rotationAxis)
+            //        {
+            //            case "X":
+            //                offset = offset - new Vector3(1, 0, 0);
+            //                break;
+            //            case "Y":
+            //                offset = offset - new Vector3(0, 1, 0);
+            //                break;
+            //            case "Z":
+            //                offset = offset - new Vector3(0, 0, 1);
+            //                break;
+            //        }
+            //    }
+
+            //    Logger.LogInfo($"Offset: {offset}");
+        }
+    }
+}
